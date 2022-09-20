cf3cd0864b3432bb1471d25eb095b8b80621ba95
RAJVI CHOKSHI <89914047+D21IT182@users.noreply.github.com>
2022-09-20 12:43:26 +0530

    updatestudent

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       
       <Routes>
       <Route path="/addStudent" element={<Student/>}/>
+      <Route path="/updateStudent/:id" element={<Student/>}/>
       <Route path="/viewStudent" element={<Viewstudent/>}/>
       </Routes>
       
